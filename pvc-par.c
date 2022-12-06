a19d3b740a1a615bed8494b7e07c5c65bcddeeab
jlrconst <jlconsttantino@live.com>
2022-12-06 11:14:11 -0300

    fixed example and added executables to gitignore

--- pvc-par.c
+++ pvc-par.c
@@ -2,11 +2,11 @@
 #include <mpi.h>
 
 int main(int argc, char** argv){
-    int process_Rank, size_Of_Cluster;
+    int process_rank, size_Of_Cluster;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
 
     printf("Hello World from process %d of %d\n", process_rank, size_Of_Cluster);
 
